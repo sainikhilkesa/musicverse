f33ee50df2b1783777439b15342c5ccd220c7ca5
yashwanthkosuri <yashwanthkosuri29@gmail.com>
2023-10-01 16:02:44 +0530

    improvements

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,11 +5,10 @@ var ctrlOthers = require('../controllers/others');
 /* Locations pages */
 router.get('/', ctrlCatalogue.homelist);
 router.get('/songs/review/new', ctrlCatalogue.addReview);
-router.get('/songs/I Wanna Be Yours', ctrlCatalogue.songsInfo); 
-router.get('/songs/Starboy', ctrlCatalogue.songsInfo1); 
-router.get('/songs/Levitating', ctrlCatalogue.songsInfo2); 
-router.get('/songs/Sunflower-Spider-Man: Into the Spider-Verse', ctrlCatalogue.songsInfo3); 
-router.get('/songs/Let Her Go', ctrlCatalogue.songsInfo4); 
+router.get('/songs/I-Wanna-Be-Yours', ctrlCatalogue.songsInfo1); 
+router.get('/songs/Starboy', ctrlCatalogue.songsInfo2); 
+router.get('/songs/Levitating', ctrlCatalogue.songsInfo3); 
+router.get('/songs/Sunflower-Spider-Man:-Into-the-Spider-Verse', ctrlCatalogue.songsInfo4); 
 router.get('/songs/songs-catalogue', ctrlCatalogue.catalogue);
 
 
